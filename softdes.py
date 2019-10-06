9ac35ac64f8db5ba83106e23d05bd1675508a444
Pedro Cunial <pedrocunial@gmail.com>
2019-10-06 13:39:30 -0300

    daijobu

--- softdes.py
+++ softdes.py
@@ -1,42 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Servidor De Desafios
-**************************
-
-O que é?
-==========================
-
-Trata-se de um projeto desenvolvido pelo professor Raul Ikeda
-para a matéria de design de software do primeiro semestre
-do Insper.
-
-O projeto possibilita que os alunos desenvolvam ou façam o upload
-de soluções para problemas de sala de aula, o qual, por sua vez
-é rodado em um lambda da AWS onde é avaliado.
-
-Instalação
-==========================
-
-O projeto foi realizado em Python 3, utilizando uma DB em SQLite 3
-(a qual já faz parte da biblioteca padrão do próprio python 3)
-
-Além disso, o projeto depende das seguintes bibliotecas externas:
-
-* Flask
-* flash_httpauth
-
-Uso
-==========================
-
-Por fim, para executar o projeto, é necessário criar um arquivo
-user.csv, e rodar o script adduser.py, o qual criará os usuários
-descritos a partir do csv.
-
-O padrão do csv é que cada linha contenha o usuário de um aluno
-e o seu tipo (sendo que usuários chamados "fabioja" ou com o tipo)
-admin terão permissões especiais.
-
-"""
 
 from flask import Flask, request, jsonify, abort, make_response, session, render_template
 from flask_httpauth import HTTPBasicAuth
